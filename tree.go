9c9250640837ca98361b6e69d399e0a1870052d2
Jetvp <greatview@gmail.com>
2013-07-20 20:55:55 +1000

    Support multiple wildcard elements per path element padding with multiple items.

--- tree.go
+++ tree.go
@@ -35,12 +35,11 @@ import (
 )
 
 type Node struct {
-	edges    map[string]*Node // the various path elements leading out of this node.
-	prefix   map[string]*Node // the various path prefixes leading out of this node.
-	wildcard *Node            // if set, this node had a wildcard as its path element.
-	leaf     *Leaf            // if set, this is a terminal node for this leaf.
-	star     *Leaf            // if set, this path ends in a star.
-	leafs    int              // counter for # leafs in the tree
+	edges     map[string]*Node    // the various path elements leading out of this node.
+	wildcards map[string]Wildcard // if set, this slice holds nodes for paths with wildcard elements.
+	leaf      *Leaf               // if set, this is a terminal node for this leaf.
+	star      *Leaf               // if set, this path ends in a star.
+	leafs     int                 // counter for # leafs in the tree
 }
 
 type Leaf struct {
@@ -49,9 +48,23 @@ type Leaf struct {
 	order     int         // the order this leaf was added
 }
 
+type Wildcard struct {
+	node     *Node    // node for this wildcard element
+	padding  []string // padding elements between each var
+	wildend  bool     // if it ends with a wildcard
+	minorder int      // minimum order value in this path
+}
+
 // New returns a new path tree.
 func New() *Node {
-	return &Node{edges: make(map[string]*Node), prefix: make(map[string]*Node)}
+	return &Node{edges: make(map[string]*Node), wildcards: make(map[string]Wildcard)}
+}
+
+// Adds a new wildcard element to the node and returns the node
+func (n *Node) addWildcard(padding []string, representation string, wildend bool, order int) *Node {
+	element := Wildcard{node: New(), padding: padding, wildend: wildend, minorder: order}
+	n.wildcards[representation] = element
+	return element.node
 }
 
 // Add a path and its associated value to the tree.
@@ -82,14 +95,8 @@ func (n *Node) add(order int, elements, wildcards []string, val interface{}) err
 	var el string
 	el, elements = elements[0], elements[1:]
 
-	// Handle wildcards.
-	switch el[0] {
-	case ':':
-		if n.wildcard == nil {
-			n.wildcard = New()
-		}
-		return n.wildcard.add(order, elements, append(wildcards, el[1:]), val)
-	case '*':
+	// Handle stars
+	if len(el) > 0 && el[0] == '*' {
 		if n.star != nil {
 			return errors.New("duplicate path")
 		}
@@ -101,15 +108,45 @@ func (n *Node) add(order int, elements, wildcards []string, val interface{}) err
 		return nil
 	}
 
-	// Handle prefixes
+	// Handle wildcards
 	if pos := strings.LastIndex(el, ":"); pos != -1 {
-		e, ok := n.prefix[el[:pos]]
-		if !ok {
-			e = New()
-			n.prefix[el[:pos]] = e
+		// Remove any final closing :
+		if el[len(el)-1] == ':' {
+			el = el[:len(el)-1]
+		}
+		parts := strings.Split(el, ":")
+		paddings := make([]string, len(parts)/2+len(parts)%2)
+		variables := make([]string, len(parts)/2)
+
+		// Split appart padding and variables (padding first even if empty)
+		for key, value := range parts {
+			if key%2 == 0 {
+				paddings[key/2] = value
+			} else {
+				variables[key/2] = value
+			}
 		}
 
-		return e.add(order, elements, append(wildcards, el[pos+1:]), val)
+		// Create string representation for map
+		wildend := len(paddings) == len(variables)
+		representation := strings.Join(paddings, ":")
+		if wildend {
+			representation = representation + ":"
+		}
+
+		// Test if map contains representation else create it
+		item, ok := n.wildcards[representation]
+		var node *Node
+		if ok {
+			node = item.node
+			if item.minorder > order {
+				item.minorder = order
+			}
+		} else {
+			node = n.addWildcard(paddings, representation, wildend, order)
+		}
+
+		return node.add(order, elements, append(wildcards, variables...), val)
 	}
 
 	// It's a normal path element.
@@ -147,23 +184,50 @@ func (n *Node) find(elements, exp []string) (leaf *Leaf, expansions []string) {
 	var el string
 	el, elements = elements[0], elements[1:]
 	if nextNode, ok := n.edges[el]; ok {
-		leaf, expansions = nextNode.find(elements, exp)
-	}
-
-	// Handle prefixes
-	for key, nextNode := range n.prefix {
-		if strings.HasPrefix(el, key) {
-			leaf, expansions = nextNode.find(elements, append(exp, el[len(key):]))
-			break
+		if testleaf, testexpansions := nextNode.find(elements, exp); testleaf != nil {
+			leaf, expansions = testleaf, testexpansions
 		}
 	}
 
-	// Handle colon
-	if n.wildcard != nil {
-		wildcardLeaf, wildcardExpansions := n.wildcard.find(elements, append(exp, el))
-		if wildcardLeaf != nil && (leaf == nil || leaf.order > wildcardLeaf.order) {
-			leaf = wildcardLeaf
-			expansions = wildcardExpansions
+	// Handle wildards
+	for _, value := range n.wildcards {
+		// Only check if tree contrains lower order item
+		if leaf != nil && leaf.order < value.minorder {
+			continue
+		}
+
+		found := bool(true)
+		variables := make([]string, 0, 0)
+		input := el
+
+		// Check all padding elements are present
+		for count, pad := range value.padding {
+			pos := strings.Index(input, pad)
+
+			if (pos == -1) || (count == 0 && pos > 0) {
+				found = false
+				break
+			}
+
+			if count != 0 {
+				variables = append(variables, input[:pos])
+			}
+			input = input[pos+len(pad):]
+		}
+
+		if !found {
+			continue
+		}
+
+		if value.wildend {
+			variables = append(variables, input)
+		}
+
+		// Set leaf if it meets lower levels
+		if testleaf, testexpansions := value.node.find(elements, append(exp, variables...)); testleaf != nil {
+			if leaf == nil || leaf.order > testleaf.order {
+				leaf, expansions = testleaf, testexpansions
+			}
 		}
 	}
 
